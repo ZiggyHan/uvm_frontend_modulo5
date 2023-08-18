7798611bc617baf29e1a0a70d9c0eb36ac09f439
ZiggyHan <alexi.numat@gmail.com>
2023-08-18 13:26:43 -0600

    module_5

---
+++ modulo_5/src/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import './estilo.css';
+import Imagen from './hajime-no-ippo-manga.jpg';
+import Datos from './datos.csv';
+import yaml from './datos.yaml';
+import json5 from './datos.json5';
+import './estilo.scss';
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', ()=>{
+        navigator.serviceWorker.register('./service-worker.js').then(registration =>{
+            console.log('SW registrado', registration);
+        }).catch(err=>{
+            console.log('SW no registrado', err)
+        });
+    });
+}
+
+function componente() {
+    const elemento = document.createElement('div')
+    //lodash
+    elemento.innerHTML = _.join(['Hola', 'Webpack'], ' ')
+    elemento.classList.add('fondo');
+    const miImagen = new Image()
+    miImagen.src = Imagen;
+    elemento.appendChild(miImagen);
+
+    console.log(Datos);
+    return elemento;
+}
+
+console.log(yaml.title)
+console.log(json5.owner.name)
+
+document.body.appendChild(componente())
+document.body.classList.add('fondo')
